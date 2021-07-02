e75774ee37869b7550d11df873e6605092758f92
Pyon18Pyon <ayasayshi@gmail.com>
2021-07-02 17:12:50 +1000

    feat: add react-color

--- src/components/Brush.jsx
+++ src/components/Brush.jsx
@@ -1,18 +1,54 @@
-import React from 'react';
-import styles from './Brush.module.css';
-import cx from 'classnames'
+import React from "react";
+import { SketchPicker } from "react-color";
+import styles from "./Brush.module.css";
+import cx from "classnames";
 
-function Brush() {
-  
-  // classnames
+function Brush({
+  handleColorCode,
+  brushColor,
+  showBrushColorPicker,
+  isBrushColorClicked,
+  inputChangeForBrush,
+  handleSliderChange,
+  sliderSize
+}) {
+
+  // classnames for styling
   const brushTool = cx(styles.brush, styles.tool);
-  
+
+  const brushColorHex = brushColor.toUpperCase();
+
   return (
     <div className={brushTool}>
-      <i className={`${styles.fas} fas fa-brush `}  id="brush" title="Brush"></i>
-      <input className="jscolor" value="a51dab" id="brush-color" />
-      <span className={styles.size} id="brush-size" title="Brush Size">10</span>
-      <input type="range" min="1" max="50" value="10" className={styles.slider} id="brush-slider" />
+      <i className={`${styles.fas} fas fa-brush `} id="brush" title="Brush"></i>
+      <input
+        className={styles.labelStyle}
+        type="text"
+        value={brushColorHex}
+        onClick={showBrushColorPicker}
+        style={{ backgroundColor: brushColor }}
+        onChange={inputChangeForBrush}
+      />
+      <div className={styles.colorPalette}>
+        {isBrushColorClicked && (
+          <SketchPicker 
+            color={brushColor} 
+            onChange={handleColorCode}
+          />
+        )}
+      </div>
+      <span className={styles.size} id="brush-size" title="Brush Size">
+        {sliderSize < 10 ? `0${sliderSize}` : sliderSize}
+      </span>
+      <input
+        type="range"
+        min="1"
+        max="50"
+        defaultValue={sliderSize}
+        className={styles.slider}
+        id="brush-slider"
+        onChange={handleSliderChange}
+      />
     </div>
   );
 }
--- src/components/Bucket.jsx
+++ src/components/Bucket.jsx
@@ -1,15 +1,43 @@
-import React from 'react';
-import styles from './Bucket.module.css';
-import cx from 'classnames';
+import React from "react";
+import { SketchPicker } from "react-color";
+import styles from "./Bucket.module.css";
+import cx from "classnames";
 
-function Bucket() {
+function Bucket({
+  bucketColor,
+  handleColorCode,
+  showBucketColorPicker,
+  isBucketColorClicked,
+  inputChangeForBucket
+}) {
 
-  // classnames
+  // classnames for styling
   const bucketTool = cx(styles.bucket, styles.tool);
+
+  const bucketColorHex = bucketColor.toUpperCase();
+
   return (
     <div className={bucketTool}>
-      <i className={`${styles.fas} fas fa-fill-drip`} title="Background Color"></i>
-      <input className="jscolor" value="ffffff" id="bucket-color" />
+      <i
+        className={`${styles.fas} fas fa-fill-drip`}
+        title="Background Color"
+      ></i>
+      <input
+        className={styles.labelStyle}
+        value={bucketColorHex}
+        id="bucket-color"
+        onClick={showBucketColorPicker}
+        style={{ backgroundColor: bucketColor }}
+        onChange={inputChangeForBucket}
+      />
+      <div className={styles.colorPalette}>
+        {isBucketColorClicked && (
+          <SketchPicker 
+            color={bucketColor} 
+            onChange={handleColorCode} 
+          />
+        )}
+      </div>
     </div>
   );
 }
